2073831c75e6a349dcbbafd84fe8adc663653e52
muhsin27 <muhsin.aziz027@gmail.com>
2024-02-22 22:59:50 +0800

    Tugasan 1 - Done

--- wordle.js
+++ wordle.js
@@ -1,34 +1,45 @@
-
 // get list of all 5 letter words
-const allWords = require('./words');
+const allWords = require("./words");
 // has these letters
-const hasLetters = ['e','s','i']; 
+const hasLetters = ["p", "l", "u", "m"];
 // must not has these letters
-const mustNotHaveLetters = ['r', 'a','d','y','b','l','u','v'];
+const mustNotHaveLetters = ["h", "o", "s", "e", "i", "n", "t", "b"];
 // must have selected letters in these positions
-const mustHaveLettersInPosition = ['s','p','i',,'e']
+const mustHaveLettersInPosition = [, , , ,];
 // not in these positions
-const mustNotHaveLettersInPosition = [,'e',,'c','s']
+const mustNotHaveLettersInPosition = ["p", "l", "u", "m"];
 
 const containsLetter = (word, letter) => {
   return word.includes(letter);
-}
+};
 
 const doesNotContainLetter = (word, letter) => {
   return !word.includes(letter);
-}
+};
 
 const hasLetterInPosition = (word, letter, position) => {
   if (letter === undefined) return true;
 
   return word[position] === letter;
-}
+};
 
-const filteredWords = allWords.filter(word => {
-  return hasLetters.every(letter => containsLetter(word, letter)) &&
-  mustNotHaveLetters.every(letter => doesNotContainLetter(word, letter)) &&
-  mustHaveLettersInPosition.every((letter, index) => hasLetterInPosition(word, letter, index))
-})
+const letterNotInPosition = (word, letter, position) => {
+  if (letter === undefined) return true;
+  return !(word[position] === letter);
+};
+
+const filteredWords = allWords.filter((word) => {
+  return (
+    hasLetters.every((letter) => containsLetter(word, letter)) &&
+    mustNotHaveLetters.every((letter) => doesNotContainLetter(word, letter)) &&
+    mustHaveLettersInPosition.every((letter, index) =>
+      hasLetterInPosition(word, letter, index)
+    ) &&
+    mustNotHaveLettersInPosition.every((letter, index) =>
+      letterNotInPosition(word, letter, index)
+    )
+  );
+});
 
 console.log(filteredWords);
-console.log('possible words', filteredWords.length);
+console.log("possible words", filteredWords.length);
